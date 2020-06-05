35a772c75811e1bbb734444474f85e77dbf0399e
kdsalvy <kedia.saurabh@gmail.com>
2020-06-05 11:56:27 +0530

    fixed errors and added ingress file

--- docker-kubernetes-springboot-demo/docker-kubernetes-springboot-demo/src/main/java/mini/project/spring/controller/RestAPIController.java
+++ docker-kubernetes-springboot-demo/docker-kubernetes-springboot-demo/src/main/java/mini/project/spring/controller/RestAPIController.java
@@ -1,9 +1,11 @@
 package mini.project.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/rest/application")
+@RestController
+@RequestMapping( path = "/rest/application")
 public class RestAPIController {
 
     @GetMapping
